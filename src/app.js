600264fb959297d5fb5b939e341a74901f938377
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-04 14:55:21 +0900

    refactor: 에러 코드 상수화

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import videoRoutes from "./routes/videoRoutes.js";
 import env from "./config/env.js";
-import { MESSAGES } from "./config/constants.js";
+import { HTTP_STATUS, MESSAGES } from "./config/constants.js";
 
 const app = express();
 
@@ -11,11 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(`${env.API_PREFIX}/video`, videoRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: MESSAGES.ERROR.NOT_FOUND_PAGE });
+  res
+    .status(HTTP_STATUS.NOT_FOUND)
+    .json({ message: MESSAGES.ERROR.NOT_FOUND_PAGE });
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
+  res.status(err.status || HTTP_STATUS.SERVER_ERROR).json({
     message: err.message || MESSAGES.ERROR.SERVER_ERROR,
     ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
