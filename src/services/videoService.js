c55a8c09265ccc9955975a0e5065cd21a95b0b8f
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-04 16:13:11 +0900

    refactor: 불필요한 try/catch 구문 제거

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,42 +1,35 @@
 import ytdl from "@distube/ytdl-core";
 import { Storage } from "@google-cloud/storage";
+
 import env from "../config/env.js";
 
 const getYoutubeVideo = async (youtubeUrl) => {
-  try {
-    const videoStream = ytdl(youtubeUrl, { quality: "highest" });
+  const videoStream = ytdl(youtubeUrl, { quality: "highest" });
 
-    return videoStream;
-  } catch (error) {
-    throw error;
-  }
+  return videoStream;
 };
 
 const saveVideoToGcs = async (videoStream, fileOutputName) => {
-  try {
-    const storage = new Storage({
-      projectId: env.googleProjectId,
-      keyFilename: env.keyFileName,
-      metadata: {
-        contentType: "video/mp4",
-      },
-    });
-    const bucket = storage.bucket(env.bucketName);
-    const file = bucket.file(fileOutputName);
+  const storage = new Storage({
+    projectId: env.googleProjectId,
+    keyFilename: env.keyFileName,
+    metadata: {
+      contentType: "video/mp4",
+    },
+  });
+  const bucket = storage.bucket(env.bucketName);
+  const file = bucket.file(fileOutputName);
 
-    await new Promise((resolve, reject) => {
-      videoStream
-        .pipe(file.createWriteStream({ contentType: "video/mp4" }))
-        .on("finish", () => {
-          resolve();
-        })
-        .on("error", (err) => {
-          reject(err);
-        });
-    });
-  } catch (error) {
-    throw error;
-  }
+  await new Promise((resolve, reject) => {
+    videoStream
+      .pipe(file.createWriteStream({ contentType: "video/mp4" }))
+      .on("finish", () => {
+        resolve();
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
 };
 
 export { getYoutubeVideo, saveVideoToGcs };
