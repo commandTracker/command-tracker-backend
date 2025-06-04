b96d8421a5787eb7639adac9ee313972750b7a61
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-04 21:49:30 +0900

    refactor: 재사용성을 위해 storage, bucket 초기화 코드 파일로 추출

--- src/config/env.js
+++ src/config/env.js
@@ -7,6 +7,8 @@ const config = {
   rabbitmqUrl: process.env.RABBITMQ_URL,
   node_env: process.env.NODE_ENV || "development",
   API_PREFIX: process.env.API_PREFIX || "/api",
+  ORIGINAL_PREFIX: process.env.ORIGINAL_PREFIX || "original",
+  EDITED_PREFIX: process.env.EDITED_PREFIX || "edited",
   fastapiUrl: process.env.FASTAPI_URL || "http://localhost:8000",
   googleProjectId: process.env.PROJECT_ID,
   keyFileName: process.env.KEY_FILE_NAME,
