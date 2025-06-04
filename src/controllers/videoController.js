1b94637051362d0f00503ec83ed05d5dc553169b
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-04 15:53:59 +0900

    feat: gcs에 video 저장

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,19 @@
+import { HTTP_STATUS, MESSAGES } from "../config/constants.js";
 import { getYoutubeVideo, saveVideoToGcs } from "../services/videoService.js";
 
 const uploadVideoRequests = async (req, res, next) => {
-  const youtubeUrl = req.query.youtubeUrl;
+  const youtubeUrl = req.body.youtubeUrl;
 
   try {
     const videoStream = await getYoutubeVideo(youtubeUrl);
+    const outputFileName = new Date().toISOString() + "video.mp4";
+    const signedUrl = await saveVideoToGcs(videoStream, outputFileName);
 
-    res.status(201).json(response);
+    res.json({
+      status: HTTP_STATUS.CREATED,
+      message: MESSAGES.SUCCESS.VIDEO_LOAD,
+      download_url: signedUrl,
+    });
   } catch (err) {
     next(err);
   }
--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,4 +1,6 @@
 import ytdl from "@distube/ytdl-core";
+import { Storage } from "@google-cloud/storage";
+import env from "../config/env.js";
 
 const getYoutubeVideo = async (youtubeUrl) => {
   try {
@@ -10,4 +12,31 @@ const getYoutubeVideo = async (youtubeUrl) => {
   }
 };
 
-export { getYoutubeVideo };
+const saveVideoToGcs = async (videoStream, fileOutputName) => {
+  try {
+    const storage = new Storage({
+      projectId: env.googleProjectId,
+      keyFilename: env.keyFileName,
+      metadata: {
+        contentType: "video/mp4",
+      },
+    });
+    const bucket = storage.bucket(env.bucketName);
+    const file = bucket.file(fileOutputName);
+
+    await new Promise((resolve, reject) => {
+      videoStream
+        .pipe(file.createWriteStream({ contentType: "video/mp4" }))
+        .on("finish", () => {
+          resolve();
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { getYoutubeVideo, saveVideoToGcs };
