76f85b6cd0fb377a18bfc39b9df654154069e5bb
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-06 20:50:00 +0900

    feat: nodemailer로 이메일 전송 로직 구현

---
+++ src/services/emailService.js
@@ -0,0 +1,39 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import ejs from "ejs";
+import nodemailer from "nodemailer";
+
+import config from "../../config/env.js";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: config.email_user,
+    pass: config.email_pass,
+  },
+});
+
+const readEmailTemplate = async (data) => {
+  const filePath = path.join(dirname, "../views/emailTemplate.ejs");
+  const template = await fs.readFile(filePath, "utf-8");
+  const htmlContent = ejs.render(template, data);
+  return htmlContent;
+};
+
+const sendEmail = async ({ to, subject, downloadLink }) => {
+  const html = await readEmailTemplate({ downloadLink });
+  const mailOptions = {
+    from: config.email_user,
+    to,
+    subject,
+    html,
+  };
+  await transporter.sendMail(mailOptions);
+};
+
+export { sendEmail, readEmailTemplate };
