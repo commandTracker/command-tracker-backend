17731bc079f806ed6bbe2be010c1cf3882048c91
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-06 20:52:45 +0900

    style: eslint 설정에 맞는 import 방식으로 변경

--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 
-import * as emailController from "../controllers/emailController.js";
+import notifySubmissionSuccess from "../controllers/emailController.js";
 
 const router = express.Router();
 
-router.get("/edit/submit", emailController.notifySubmissionSuccess);
+router.get("/edit/submit", notifySubmissionSuccess);
 
 export default router;
