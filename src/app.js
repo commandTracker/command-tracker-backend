2fe9eb549963da8c29d22b694444e1a0a51fe9f5
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-08 02:51:00 +0900

    feat: app.js 파일 cors 설정, 에러 메시지 상수 처리

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
+import cors from "cors";
 import express from "express";
+import createError from "http-errors";
+
+import { HTTP_STATUS, MESSAGES } from "../config/constants.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: "아무것도 없습니다." });
+  next(createError(HTTP_STATUS.NOT_FOUND, MESSAGES.ERROR.NOT_FOUND_PAGE));
 });
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
-    message: err.message || "서버 내부 오류가 발생했습니다.",
+    message: err.message || MESSAGES.ERROR.SERVER_ERROR,
     ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
 });
