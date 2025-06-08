0704df0529841c3fee81619bc019d7e5e8ad600f
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-08 20:00:10 +0900

    chore: eslint 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
+    "consistent-return": "off",
     "no-return-await": "off",
     "no-useless-catch": "off",
     "no-console": "error",
