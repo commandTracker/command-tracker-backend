66ca5318775fdd88a038bb7bdd0aebf1b89293a9
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-08 20:26:58 +0900

    feat: editController 구현

---
+++ src/routes/controllers/editController.js
@@ -0,0 +1,36 @@
+import createError from "http-errors";
+
+import { HTTP_STATUS, MESSAGES } from "../../config/constants.js";
+import transVideoAndUpload from "../../services/transVideoService.js";
+import { publishToQueue } from "../../utils/rabbitmqService.js";
+
+const editController = async (req, res, next) => {
+  const { videoSrc, trim, email, side } = req.body;
+  const [start, end] = trim.map(Number);
+
+  try {
+    const message = await transVideoAndUpload({
+      videoSrc,
+      start,
+      end,
+      email,
+      side,
+    });
+
+    const queue = "analyze_queue";
+
+    await publishToQueue(queue, message);
+
+    return res.status(HTTP_STATUS.OK).json({
+      status: HTTP_STATUS.CREATED,
+      message: MESSAGES.SUCCESS.VIDEO_REQUEST,
+      email,
+    });
+  } catch (err) {
+    return next(
+      createError(HTTP_STATUS.SERVER_ERROR, MESSAGES.ERROR.SERVER_ERROR)
+    );
+  }
+};
+
+export default editController;
