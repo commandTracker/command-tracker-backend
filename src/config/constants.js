8326c293d18e8db8597a82993fa08092c43c4bd5
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-08 20:52:47 +0900

    chore: 상수 메시지 추가

--- src/config/constants.js
+++ src/config/constants.js
@@ -19,6 +19,7 @@ const MESSAGES = {
     INACCESSIBLE_VIDEO: "접근이 제한된 영상입니다.",
     INVALID_VIDEO_TYPE: "지원하지 않는 유형의 영상입니다.",
 
+    FAILED_READ_VIDEO: "파일을 불러오지 못했습니다",
     FAILED_CONSUME_EMAIL_QUEUE: "이메일 큐 처리 실패",
     FAILED_PUBLISH_MESSAGE: "메시지 발행에 실패했습니다.",
     FAILED_READ_TEMPLATE: "이메일 템플릿을 읽지 못했습니다.",
