1c2241643eab8cc74286f3ed191f12214bd56d78
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-16 21:36:00 +0900

    feat: mjml 형식의 이메일 템플릿 적용

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 
 import ejs from "ejs";
 import createError from "http-errors";
+import mjml2html from "mjml";
 import nodemailer from "nodemailer";
 
 import { MESSAGES } from "../config/constants.js";
@@ -23,17 +24,23 @@ const transporter = nodemailer.createTransport({
 const readEmailTemplate = async ({ message, url }) => {
   try {
     if (url === "") {
-      const filePath = path.join(dirname, "../views/errorTemplate.ejs");
+      const filePath = path.join(dirname, "../views", "errorTemplate.mjml.ejs");
       const template = await fs.readFile(filePath, "utf-8");
       const htmlContent = ejs.render(template, { message });
+      const { html } = mjml2html(htmlContent, {
+        validationLevel: "strict",
+      });
 
-      return htmlContent;
+      return html;
     }
-    const filePath = path.join(dirname, "../views/emailTemplate.ejs");
+    const filePath = path.join(dirname, "../views", "emailTemplate.mjml.ejs");
     const template = await fs.readFile(filePath, "utf-8");
-    const htmlContent = ejs.render(template, { message, url });
+    const htmlContent = ejs.render(template, { url });
+    const { html } = mjml2html(htmlContent, {
+      validationLevel: "strict",
+    });
 
-    return htmlContent;
+    return html;
   } catch (err) {
     throw createError.InternalServerError(MESSAGES.ERROR.FAILED_READ_TEMPLATE);
   }
@@ -45,7 +52,7 @@ const sendEmail = async ({ email, message, url }) => {
     const mailOptions = {
       from: config.email_user,
       to: email,
-      subject: "Command Tracker",
+      subject: "Command Tracker ddd",
       html,
     };
 
