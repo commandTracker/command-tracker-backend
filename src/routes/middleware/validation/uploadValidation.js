3cb6288c5027434dc51a834484de3ad7c09e72fb
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-18 20:55:40 +0900

    feat: 배포를 위해 ytdl 검증 제거

--- src/routes/middleware/validation/uploadValidation.js
+++ src/routes/middleware/validation/uploadValidation.js
@@ -4,16 +4,6 @@ import createError from "http-errors";
 import { MESSAGES, REQUIRED_FIELDS } from "../../../config/constants.js";
 import { validateReqBody, validateFields } from "../../../utils/validation.js";
 
-const validateMetadata = (metaData) => {
-  const { isLiveContent, isShortsEligible } = metaData;
-
-  if (isLiveContent || isShortsEligible) {
-    const error = new createError.BadRequest(MESSAGES.ERROR.INVALID_VIDEO_TYPE);
-
-    throw error;
-  }
-};
-
 const validateUploadUrl = async (req, res, next) => {
   try {
     validateReqBody(req.body);
@@ -30,21 +20,8 @@ const validateUploadUrl = async (req, res, next) => {
       throw error;
     }
 
-    const videoInfo = await ytdl.getBasicInfo(youtubeUrl);
-
-    validateMetadata(videoInfo.videoDetails);
-
     next();
   } catch (error) {
-    if (
-      error.message.includes("Sign in to confirm your age") ||
-      error.message.includes("Video unavailable")
-    ) {
-      next(new createError.BadRequest(MESSAGES.ERROR.INVALID_VIDEO_TYPE));
-    } else if (error.message.includes("This is a private video")) {
-      next(new createError.Forbidden(MESSAGES.ERROR.INACCESSIBLE_VIDEO));
-    }
-
     next(error);
   }
 };
